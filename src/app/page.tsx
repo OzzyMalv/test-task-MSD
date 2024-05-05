1b8238edb2e7dedb2fd65f53a51f81cded9b8958
Alex_Malv <a.malinovsky303@gmail.com>
2024-05-05 20:24:32 +0200

    feat(charts): data fetch and charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import {HeaderView} from "~/app/_components/HeaderView";
 import {GraphsView} from "~/app/_components/GraphsView";
+import {api} from "~/trpc/server";
 
 export default async function Home() {
+  const covidCases = await api.positiveCovidCases.getAll()
+  const virusCases = await api.positiveVirusCases.getAll()
+
   return (
     <main className="flex h-screen flex-col">
       <HeaderView />
 
       <div className="h-screen bg-gray-50">
-        <GraphsView />
+        <GraphsView covidCases={covidCases} virusCases={virusCases} />
       </div>
     </main>
   );
